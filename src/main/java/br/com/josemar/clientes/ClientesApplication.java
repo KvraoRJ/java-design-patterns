37c7edcb972b0232ce3f3995500fffff5d0e2c6d
Josemar Pires <josemarpires@hotmail.com>
2024-12-23 08:46:29 -0300

    first commit

---
+++ src/main/java/br/com/josemar/clientes/ClientesApplication.java
@@ -0,0 +1,15 @@
+package br.com.josemar.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/josemar/clientes/model/Cliente.java
@@ -0,0 +1,61 @@
+package br.com.josemar.clientes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String telefone;
+	private String email;
+	@ManyToOne
+	private Endereco endereco;
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+}
